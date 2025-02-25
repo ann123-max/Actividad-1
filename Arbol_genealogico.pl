b8d2dfeea062d7d5bbc5d8db05bfcbb65ef6dac5
ann123-max <ana.cabrales@utp.edu.co>
2025-02-24 19:25:33 -0500

    Hechos
    Se establecieron los hechos de cada relacion directa

---
+++ Arbol_genealogico.pl
@@ -0,0 +1,23 @@
+%HECHOS:
+
+%Padres:
+
+padre_de(abraham, homero).
+padre_de(abraham, herbert).
+padre_de(clancy, marge).
+padre_de(clancy, patty).
+padre_de(clancy, selma).
+padre_de(homero, bart).
+padre_de(homero, lisa).
+padre_de(homero, maggie).
+
+%Madres:
+
+madre_de(mona, homero).
+madre_de(marge, bart).
+madre_de(marge, lisa).
+madre_de(marge, maggie).
+madre_de(jacqueline, marge).
+madre_de(jacqueline, patty).
+madre_de(jacqueline, selma).
+madre_de(selma, ling).
